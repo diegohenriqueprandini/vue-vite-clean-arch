3e58954a50a43046533c8a81dc2851b251f58b8e
diego.prandini <contato.diego.prandini@gmail.com>
2023-02-14 13:11:50 -0300

    tests gpg

--- frontend/src/infra/HttpClient.ts
+++ frontend/src/infra/HttpClient.ts
@@ -1,3 +1,10 @@
 export default interface HttpClient {
     
+    get(url: string): Promise<any>
+
+    post(url: string, body: any): Promise<void>
+
+    put(url: string, body: any): Promise<void>
+
+    delete(url: string): Promise<void>
 }
